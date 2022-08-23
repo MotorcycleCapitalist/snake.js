fd80baf53c753d482cc8544bd6dc46898092707f
Chris <46506838+MotorcycleCapitalist@users.noreply.github.com>
2022-08-23 11:17:45 -0600

    play.js file upload
    connect to server exercise

---
+++ play.js
@@ -0,0 +1,11 @@
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
+
+
+console.log("Connecting ...");
+// const conn = connect();
+conn = {}
+
+setupInput(conn)
+
